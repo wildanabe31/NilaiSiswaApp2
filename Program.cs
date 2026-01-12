1593d1064364e05fc90877b928d4743f4ea80409
wildanabe31 <fadilalfadil091@gmail.com>
2026-01-12 09:51:17 +0700

    Tambah input nilai & nama

--- Program.cs
+++ Program.cs
@@ -2,3 +2,9 @@
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.WriteLine("Input Nama: ");
+nama = Console.ReadLine();
+
+Console.WriteLine("Input Nilai: ");
+nilai = int.Parse(Console.ReadLine());
